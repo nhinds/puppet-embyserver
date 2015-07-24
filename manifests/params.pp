5236676d757eb272c8d5aa3baab7a74ac11b3378
Andy Newton <amn@ecs.soton.ac.uk>
2015-07-24 22:20:44 +0100

    seemingly working emby server module, only tested on ubuntu 14.04

---
+++ manifests/params.pp
@@ -0,0 +1,27 @@
+class embyserver::params (
+) {
+
+  $package = 'emby-server'
+  $service = 'emby-server'
+
+  # Check: Only supporting debian-y things at this time...
+  case "${::lsbdistid}_${::lsbdistrelease}" {
+    'Ubuntu_12.04', 'Ubuntu_14.04', 'Ubuntu_14.10', 'Ubuntu_15.04' : {
+      $repo = "xUbuntu_${::lsbdistrelease}"
+    }
+    /'Debian_7'/ : {
+      $repo = "Debian_7.0"
+    }
+    /'Debian_8'/ : {
+      $repo = "Debian_8.0"
+    }
+    default : {
+      fail("Module ${module_name} is not supported on ${::lsbdistid} ${::lsbdistrelease}")
+    }
+
+    default : {
+      fail("Module ${module_name} is not supported on ${::lsbdistid}")
+    }
+  }
+
+}
