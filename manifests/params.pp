90b3883cb2800d0cb0604ce1d7080f057bd08057
Nicholas Hinds <hindsn@gmail.com>
2017-12-02 14:50:57 -0700

    Add repository support for Ubuntu 16.04/16.10/17.04

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,7 @@ class embyserver::params (
 
   # Check: Only supporting debian-y things at this time...
   case "${::lsbdistid}_${::lsbdistrelease}" {
-    'Ubuntu_12.04', 'Ubuntu_14.04', 'Ubuntu_14.10', 'Ubuntu_15.04' : {
+    'Ubuntu_12.04', 'Ubuntu_14.04', 'Ubuntu_14.10', 'Ubuntu_15.04', 'Ubuntu_16.04', 'Ubuntu_16.10', 'Ubuntu_17.04' : {
       $repo = "xUbuntu_${::lsbdistrelease}"
     }
     /'Debian_7'/ : {
